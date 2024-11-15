46fac7191402f8bcb3f8f1c6b1e3d48d3b82baa9
Viníw <viniw.md@gmail.com>
2024-11-14 23:25:24 -0300

    criacao do prototipo

---
+++ pesquisa.py
@@ -0,0 +1,18 @@
+print("{Conversa não inicializada, digite sua mensagem para iniciar}")
+input("")
+
+print()
+print()
+print("Olá, eu sou o assistente virtual e estou aqui para te ajudar!  😊")
+print("Para iniciarmos o atendimento, preciso saber com quem estou falando")
+nome = input("Digite seu nome")
+
+print()
+print("Ola " , nome, "em que posso ajudá-lo?")
+
+print()
+print("1 - Dúvida ou reclamação")
+print("2 - Problemas de acesso")
+print("3 - Não sei o que")
+opcao_selecionada  = input("Digite seu nome")
+# print("Você digitou:", entrada_usuario)ffsdfsdfsd
